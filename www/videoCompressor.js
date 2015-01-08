701017e2d48266811d1a5da79fb94322efc4a555
AsmitaAK <asmita.koli@gmail.com>
2015-01-08 19:22:23 +0530

    added videocompressor.js

---
+++ www/videoCompressor.js
@@ -0,0 +1,19 @@
+
+	
+	 	var exec = require('cordova/exec') ; 
+	module.exports = {
+    start: function(successCallback, errorCallback) {
+        //exec(successCallback, errorCallback, "ContactVcardPicker", "getContactVcard", []);
+        exec(successCallback, errorCallback, 'WatermarkDetector', 'start', []);
+    }
+	
+	};
+function success(data)
+{
+alert("success"+data); 
+}
+
+ function failure(data)
+{
+alert("failure");
+}
